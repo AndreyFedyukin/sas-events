ccf0ca5018067c95c2acce56144792b8824c0ded
Andrey Fedyukin <andreynf@mail.ru>
2024-08-11 18:23:17 +0300

    work on the code

--- js/main.js
+++ js/main.js
@@ -34,63 +34,92 @@ document.body.addEventListener("click", (event) => {
 
 // Модальные окна ***************************
 //* Модальное окно для видео ***************************
-const modalVideo = document.querySelector(".my-modal-video");
-const video = document.querySelector(".my-video");
+const modalVideo = document.querySelectorAll("[data-modal-video]");
+const video = document.querySelectorAll(".my-video");
 const body = document.body;
 
-document
-  .querySelector(".open-modal-btn-video")
-  .addEventListener("click", () => {
-    modalVideo.classList.add("open");
-    video.play();
+document.querySelectorAll("[data-open-video]").forEach((button, index) => {
+  button.addEventListener("click", () => {
+    modalVideo[index].classList.add("open");
+    video[index].play();
     body.classList.add("disable-scroll");
   });
+});
 
-document
-  .querySelector(".close-modal-btn-video")
-  .addEventListener("click", () => {
-    modalVideo.classList.remove("open");
-    video.pause();
+document.querySelectorAll("[data-close-video]").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const target = event.target.closest("[data-modal-video]");
+    if (target) {
+      const index = Array.from(modalVideo).indexOf(target);
+      modalVideo[index].classList.remove("open");
+      video[index].pause();
+      body.classList.remove("disable-scroll");
+    }
+  });
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    modalVideo.forEach((element) => {
+      if (element.classList.contains("open")) {
+        const index = Array.from(modalVideo).indexOf(element);
+        modalVideo[index].classList.remove("open");
+        video[index].pause();
+        body.classList.remove("disable-scroll");
+      }
+    });
+  }
+});
+
+modalVideo.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    if (event.target !== event.currentTarget) return;
+    const index = Array.from(modalVideo).indexOf(element);
+    modalVideo[index].classList.remove("open");
+    video[index].pause();
     body.classList.remove("disable-scroll");
   });
+});
 
-window.addEventListener("keydown", (e) => {
+//* Модальное окно для картин ***************************
+const modalImg = document.querySelectorAll("[data-modal-img]");
+const bodes = document.body;
+
+document.querySelectorAll("[data-open-img]").forEach((button, index) => {
+  button.addEventListener("click", () => {
+    modalImg[index].classList.add("open");
+    bodes.classList.add("disable-scroll");
+  });
+});
+
+document.querySelectorAll("[data-close-img]").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const target = event.target.closest("[data-modal-img]");
+    if (target) {
+      const index = Array.from(modalImg).indexOf(target);
+      modalImg[index].classList.remove("open");
+      bodes.classList.remove("disable-scroll");
+    }
+  });
+});
+
+document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
-    modalVideo.classList.remove("open");
-    video.pause();
-    body.classList.remove("disable-scroll");
+    modalImg.forEach((element) => {
+      if (element.classList.contains("open")) {
+        const index = Array.from(modalImg).indexOf(element);
+        modalImg[index].classList.remove("open");
+        bodes.classList.remove("disable-scroll");
+      }
+    });
   }
 });
 
-modalVideo.addEventListener("click", (event) => {
-  if (event.target !== event.currentTarget) return;
-  event.currentTarget.classList.remove("open");
-  video.pause();
-  body.classList.remove("disable-scroll");
-});
-
-//* Модальное окно для картинок ***************************
-const modalImage = document.querySelector(".my-modal-img");
-
-document.querySelector(".open-modal-btn").addEventListener("click", () => {
-  modalImage.classList.add("open");
-  document.body.classList.add("disable-scroll");
-});
-
-document.querySelector(".close-my-modal-btn").addEventListener("click", () => {
-  modalImage.classList.remove("open");
-  document.body.classList.remove("disable-scroll");
-});
-
-window.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    modalImage.classList.remove("open");
-    document.body.classList.remove("disable-scroll");
-  }
-});
-
-modalImage.addEventListener("click", (event) => {
-  if (event.target !== event.currentTarget) return;
-  event.currentTarget.classList.remove("open");
-  document.body.classList.remove("disable-scroll");
+modalImg.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    if (event.target !== event.currentTarget) return;
+    const index = Array.from(modalImg).indexOf(element);
+    modalImg[index].classList.remove("open");
+    bodes.classList.remove("disable-scroll");
+  });
 });
